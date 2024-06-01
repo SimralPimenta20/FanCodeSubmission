a27ae7cf97be1bd08992614ef8e93c64fa968af9
Simral Pimenta <simralspimenta@gmail.com>
2024-06-01 18:50:13 +0530

    checking

--- Users.py
+++ Users.py
@@ -5,7 +5,7 @@ class Geo:
     def __init__(self, lat, lng):
         self.lat = lat
         self.lng = lng
-    #find whether in Fancode or not
+    ###find whether in Fancode or not
     ##def LocationFancode (self):
         ##return ((float(self.lat) > -40.00 and float(self.lat) < 5.00) and (float(self.lng) > 5.00 and float(self.lng) < 100.00))
     
